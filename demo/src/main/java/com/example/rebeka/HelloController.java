fb2244757e754655352a3877f931126c2586f1e0
Rebeka Haxhillari <rebekahaxhillari17@icloud.com>
2025-05-19 14:04:47 +0200

    U shtua projekti ne branch springboot

---
+++ demo/src/main/java/com/example/rebeka/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.rebeka;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "U krijua nje projekt me SpringBoot;
+    }
+}
